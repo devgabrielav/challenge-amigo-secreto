46b330f608f9dfe6d22ca7fcbd542167a8182cfa
Gabriela <gabrivicentini@icloud.com>
2025-01-29 17:13:47 -0300

    Adiciona funcionalidades

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+let listaAmigos = document.getElementById('listaAmigos');
+
+function adicionarAmigo() {
+  let nomeDigitado = document.querySelector('input');
+  
+  if (!nomeDigitado.value) {
+    alert('Digite um nome válido.')
+  } else {
+    amigos.push(nomeDigitado.value);
+  
+    let novoAmigo = document.createElement('li');
+    novoAmigo.textContent = nomeDigitado.value;
+    listaAmigos.appendChild(novoAmigo);
+  
+    nomeDigitado.value = '';
+  }
+}
+
+function sortearAmigo() {
+
+  let indexSorteado = Math.floor(Math.random() * amigos.length);
+  
+  let amigoSorteado = document.getElementById('resultado');
+
+  listaAmigos.hidden = true;
+
+  amigoSorteado.innerHTML = `O amigo sorteado é ${amigos[indexSorteado]}!`
+}
